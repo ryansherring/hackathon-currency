2c368f7aa1a6c03f5fca24e5b415865e70a01a8e
Ryan <ryansherring@gmail.com>
2020-06-17 13:31:52 -0700

    Updates

---
+++ precache-manifest.defab755a07e4c09950151f4c5ea6bad.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36d919f6e251ec3d12158ac9c6aacfc0",
+    "url": "/hackathon-currency/index.html"
+  },
+  {
+    "revision": "43e95b07b65c07a32d29",
+    "url": "/hackathon-currency/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9ba68ce84d7eb98b237f",
+    "url": "/hackathon-currency/static/css/main.e1a44fb4.chunk.css"
+  },
+  {
+    "revision": "43e95b07b65c07a32d29",
+    "url": "/hackathon-currency/static/js/2.17e2e65e.chunk.js"
+  },
+  {
+    "revision": "13767bda7e58db6e95d0e3d841e04d61",
+    "url": "/hackathon-currency/static/js/2.17e2e65e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ba68ce84d7eb98b237f",
+    "url": "/hackathon-currency/static/js/main.dfadb5cc.chunk.js"
+  },
+  {
+    "revision": "75e2182422fc504af919",
+    "url": "/hackathon-currency/static/js/runtime-main.121dfc56.js"
+  }
+]);
